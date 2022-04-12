c48e5dd6598074c67c3d77ee720b38be4ba1323e
Ice <1214475704@qq.com>
2022-04-12 15:09:25 +0800

    私有属性不可访问

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @Author: tangguowei
  * @Date: 2022-03-31 11:39:06
  * @LastEditors: tangguowei
- * @LastEditTime: 2022-04-12 13:49:55
+ * @LastEditTime: 2022-04-12 15:07:37
  */
 import colorPicker from 'tui-color-picker';
 import 'tui-color-picker/dist/tui-color-picker.css';
@@ -11,6 +11,7 @@ import noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 
 class TangImageEditor {
+  #wrapper = null;
   // 图片画板、最终画板
   #pictureDom = null;
   #pictureCtx = null;
@@ -44,6 +45,7 @@ class TangImageEditor {
   };
   // 是否作画过程中
   #isDrawing = false;
+  #colorDom = null;
   // 颜色选择器
   #colorPickerDom = null;
   // 颜色选择器实例
@@ -64,9 +66,9 @@ class TangImageEditor {
     this.#firstOptions.color = this.#options.color;
     this.#firstOptions.lineWidth = this.#options.lineWidth;
     if (typeof element === 'string') {
-      this.wrapper = document.querySelector(element);
+      this.#wrapper = document.querySelector(element);
     } else {
-      this.wrapper = element;
+      this.#wrapper = element;
     }
     this.setImageSrc(this.#options.imgSrc, true);
   }
@@ -88,7 +90,7 @@ class TangImageEditor {
       this.#noUiSliderInstance.set(lineWidth);
       this.#handleRest();
     }
-    const loadingDom = this.wrapper.querySelector('.tang_loading');
+    const loadingDom = this.#wrapper.querySelector('.tang_loading');
     loadingDom.style.visibility = 'visible';
     this.#options.imgSrc = url;
     const { imgSrc, maxWidth, maxHeight } = this.#options;
@@ -118,7 +120,7 @@ class TangImageEditor {
   }
   // 初始化dom
   #initDom() {
-    this.wrapper.innerHTML = `
+    this.#wrapper.innerHTML = `
       <div class="tang_submenubar">
         <svg t="1649303699973" class="icon tang_reset" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4608" width="20" height="20"><path d="M981.333333 80.64a42.666667 42.666667 0 0 0-42.666666 42.666667v106.666666A512 512 0 0 0 0 512a42.666667 42.666667 0 0 0 85.333333 0 426.666667 426.666667 0 0 1 793.386667-218.026667H768a42.666667 42.666667 0 1 0 0 85.333334h213.333333a42.666667 42.666667 0 0 0 42.666667-42.666667v-213.333333a42.666667 42.666667 0 0 0-42.666667-42.666667zM981.333333 469.333333a42.666667 42.666667 0 0 0-42.666666 42.666667A426.666667 426.666667 0 0 1 145.28 730.026667H256a42.666667 42.666667 0 0 0 0-85.333334H42.666667a42.666667 42.666667 0 0 0-42.666667 42.666667v213.333333a42.666667 42.666667 0 0 0 85.333333 0v-106.666666A512 512 0 0 0 1024 512a42.666667 42.666667 0 0 0-42.666667-42.666667z" fill="#dbdbdb" p-id="4609"></path></svg>
         <svg t="1649303143482" class="icon tang_prev" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2857" width="20" height="20"><path d="M558.08 428.3904h-360.6528l202.0352-202.0352a35.82976 35.82976 0 1 0-50.67776-50.67776l-275.15904 275.13856c-1.7408 1.76128-3.2768 3.6352-4.57728 5.62176a33.792 33.792 0 0 0-2.63168 4.99712c-0.19456 0.41984-0.48128 0.80896-0.64512 1.23904-1.49504 3.6864-2.22208 7.59808-2.43712 11.52-0.04096 0.65536-0.19456 1.30048-0.19456 1.95584 0 0.31744 0.08192 0.63488 0.09216 0.94208a35.57376 35.57376 0 0 0 10.40384 24.41216l275.15904 275.13856a35.84 35.84 0 0 0 50.67776-50.67776l-225.8944-225.8944h384.50176c186.32704 0 337.92 151.59296 337.92 337.92v3.40992a35.84 35.84 0 1 0 71.68 0v-3.40992c0-226.22208-183.38816-409.6-409.6-409.6z" fill="#dbdbdb" p-id="2858"></path></svg>
@@ -136,27 +138,27 @@ class TangImageEditor {
       <div class="tang_color_picker"></div>
       <div class="tang_loading"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none"></circle></svg></div>
     `;
-    this.wrapper.style.width = `${this.#options.maxWidth}px`;
-    this.wrapper.style.position = 'relative';
-    this.#pictureDom = this.wrapper.querySelector('.tang_picture');
+    this.#wrapper.style.width = `${this.#options.maxWidth}px`;
+    this.#wrapper.style.position = 'relative';
+    this.#pictureDom = this.#wrapper.querySelector('.tang_picture');
     this.#pictureCtx = this.#pictureDom.getContext("2d");
-    this.#canvasDom = this.wrapper.querySelector('.tang_draw');
+    this.#canvasDom = this.#wrapper.querySelector('.tang_draw');
     this.#canvasCtx = this.#canvasDom.getContext("2d");
 
     // 颜色选择器
-    this.#colorPickerDom = this.wrapper.querySelector('.tang_color_picker');
+    this.#colorPickerDom = this.#wrapper.querySelector('.tang_color_picker');
     this.#colorPickerInstance = colorPicker.create({
       container: this.#colorPickerDom,
       color: this.#options.color,
     });
-    this.colorDom = this.wrapper.querySelector('.tang_color');
+    this.#colorDom = this.#wrapper.querySelector('.tang_color');
     this.#colorPickerInstance.on('selectColor', ({ color }) => {
       this.#initColorDom(color);
     })
     this.#initSubmenuDom();
 
     // 线条宽度
-    const strokeDom = this.wrapper.querySelector('.tang_stroke');
+    const strokeDom = this.#wrapper.querySelector('.tang_stroke');
     this.#noUiSliderInstance = noUiSlider.create(strokeDom, {
       start: this.#options.lineWidth,
       range: {
@@ -165,7 +167,7 @@ class TangImageEditor {
       },
       connect: 'lower',
     });
-    this.#strokeValDom = this.wrapper.querySelector('.tang_stroke_val');
+    this.#strokeValDom = this.#wrapper.querySelector('.tang_stroke_val');
     strokeDom.noUiSlider.on('update', (values) => {
       const value = Number.parseInt(values[0]);
       this.#initLinewidthDom(value);
@@ -173,7 +175,7 @@ class TangImageEditor {
   }
   // 渲染颜色
   #initColorDom(color) {
-    this.colorDom.style.background = color;
+    this.#colorDom.style.background = color;
     this.#options.color = color;
   }
   // 渲染线条宽度
@@ -183,9 +185,9 @@ class TangImageEditor {
   }
   // 更新次级菜单状态
   #initSubmenuDom() {
-    const reset = this.wrapper.querySelector('.tang_reset');
-    const prev = this.wrapper.querySelector('.tang_prev');
-    const next = this.wrapper.querySelector('.tang_next');
+    const reset = this.#wrapper.querySelector('.tang_reset');
+    const prev = this.#wrapper.querySelector('.tang_prev');
+    const next = this.#wrapper.querySelector('.tang_next');
     const activeColor = '#515151';
     const defaultColor = '#dbdbdb';
     if (this.#historyData.length) {
@@ -212,8 +214,8 @@ class TangImageEditor {
   }
   // 初始化颜色选择器位置
   #initColorPickerPosition() {
-    this.#colorPickerDom.style.left = `${this.colorDom.offsetLeft}px`;
-    this.#colorPickerDom.style.top = `${this.colorDom.offsetTop - this.#colorPickerDom.clientHeight}px`;
+    this.#colorPickerDom.style.left = `${this.#colorDom.offsetLeft}px`;
+    this.#colorPickerDom.style.top = `${this.#colorDom.offsetTop - this.#colorPickerDom.clientHeight}px`;
   }
   // 绑定事件
   #bindAllEvents() {
@@ -234,7 +236,7 @@ class TangImageEditor {
     let target = event.target;
     let isTarget = false;
     while (target) {
-      if (target === this.colorDom) {
+      if (target === this.#colorDom) {
         this.#colorPickerDom.style.visibility = 'visible';
         isTarget = true;
         break;
@@ -284,7 +286,7 @@ class TangImageEditor {
   #handleSubmenu(event) {
     let target = event.target;
     const classes = (target.getAttribute('class') || '').split(' ');
-    if (classes.includes('active') && target.parentNode === this.wrapper.querySelector('.tang_submenubar')) {
+    if (classes.includes('active') && target.parentNode === this.#wrapper.querySelector('.tang_submenubar')) {
       if (classes.includes('tang_reset')) {
         this.#handleRest();
       } else if (classes.includes('tang_prev')) {
@@ -332,8 +334,8 @@ class TangImageEditor {
         canvas.width = width;
         canvas.height = height;
         const ctx = canvas.getContext('2d');
-        ctx.drawImage(this.#originalImage, 0, 0, width, height)
-        ctx.drawImage(img, 0, 0, width, height)
+        ctx.drawImage(this.#originalImage, 0, 0, width, height);
+        ctx.drawImage(img, 0, 0, width, height);
         const dataUrl = canvas.toDataURL('image/png');
         resolve(dataUrl);
       }
